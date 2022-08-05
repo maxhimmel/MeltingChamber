5a68eec98d068f1422677e70dcce4c3fed2db629
Max <himmelmax@gmail.com>
2022-08-05 09:22:04 -0700

    created sprite renderer color changer

---
+++ Assets/Scripts/Utility/SpriteColorRandomizer.cs
@@ -0,0 +1,54 @@
+using MeltingChamber.Extensions;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace MeltingChamber.Utility
+{
+    public class SpriteColorRandomizer : MonoBehaviour
+    {
+		[SerializeField] private bool _randomizeOnStart = true;
+
+		//[GUIColor( "GetRandomColor" )]
+		//[BoxGroup, InfoBox( "", InfoMessageType.None )]
+		[GUIColor( "GetRandomColor" )]
+		[BoxGroup, SerializeField, MinMaxSlider( 0, 1 )] private Vector2 _hueRange = new Vector2( 0, 1 );
+		[GUIColor( "GetRandomColor" )]
+		[BoxGroup, SerializeField, MinMaxSlider( 0, 1 )] private Vector2 _saturationRange = new Vector2( 0, 1 );
+		[GUIColor( "GetRandomColor" )]
+		[BoxGroup, SerializeField, MinMaxSlider( 0, 1 )] private Vector2 _valueRange = new Vector2( 0, 1 );
+		[GUIColor( "GetRandomColor" )]
+		[BoxGroup, SerializeField, MinMaxSlider( 0, 1 )] private Vector2 _alphaRange = new Vector2( 0, 1 );
+
+		private SpriteRenderer _renderer;
+
+		private void Start()
+		{
+			if ( _randomizeOnStart )
+			{
+				RandomizeColor();
+			}
+		}
+
+		[ContextMenu( "Randomize Color" )]
+		public void RandomizeColor()
+		{
+			_renderer.color = GetRandomColor();
+		}
+
+		private Color GetRandomColor()
+		{
+			return Random.ColorHSV(
+				_hueRange.Random(), _hueRange.Random(),
+				_saturationRange.Random(), _saturationRange.Random(),
+				_valueRange.Random(), _valueRange.Random(),
+				_alphaRange.Random(), _alphaRange.Random()
+			);
+		}
+
+		private void Awake()
+		{
+			_renderer = GetComponentInChildren<SpriteRenderer>();
+		}
+	}
+}
