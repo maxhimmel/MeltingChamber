bd9482b73e2f7de361a06e4319786532a3537115
Max <himmelmax@gmail.com>
2022-08-04 18:40:13 -0700

    player can dash!

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,19 +8,42 @@ namespace MeltingChamber.Gameplay.Player
     {
 		private Rewired.Player _input;
 		private CharacterMotor _motor;
+		private DashController _dashController;
+
+		private Vector2 _directedMoveInput = Vector2.right;
 
 		[Inject]
 		public void Construct( Rewired.Player input,
-			CharacterMotor motor )
+			CharacterMotor motor,
+			DashController dashController )
 		{
 			_input = input;
 			_motor = motor;
+			_dashController = dashController;
 		}
 
 		private void Update()
 		{
+			HandleMovement();
+		}
+
+		private void HandleMovement()
+		{
 			var moveInput = GetMoveInput();
-			_motor.SetDesiredVelocity( moveInput );
+			if ( moveInput != Vector2.zero )
+			{
+				_directedMoveInput = moveInput;
+			}
+
+			if ( !_dashController.IsDashing )
+			{
+				_motor.SetDesiredVelocity( moveInput );
+
+				if ( _input.GetButtonDown( ReConsts.Action.Dash ) )
+				{
+					_dashController.Dash( _directedMoveInput );
+				}
+			}
 		}
 
 		private Vector2 GetMoveInput()
