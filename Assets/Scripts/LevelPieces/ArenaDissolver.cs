0f32c40f31ef6020c1a0e3652fb600adc40e45f9
Max <himmelmax@gmail.com>
2022-08-07 08:36:54 -0700

    WIP on arena dissolving

---
+++ Assets/Scripts/LevelPieces/ArenaDissolver.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace MeltingChamber.Gameplay.LevelPieces
+{
+    public class ArenaDissolver : MonoBehaviour
+    {
+        [SerializeField] private float _rate = 10;
+        [SerializeField] private float _distanceThreshold = 0.5f;
+
+		private ArenaBuilder _builder;
+		private float _countdown = 0;
+		private float _radius;
+
+        [Inject]
+		public void Construct( ArenaBuilder builder )
+		{
+            _builder = builder;
+			_radius = builder.Radius;
+		}
+
+		private void Update()
+		{
+			_countdown += Time.deltaTime;
+			if ( _countdown >= _rate )
+			{
+				OnDissolveCountdownExpired();
+			}
+		}
+
+		private void OnDissolveCountdownExpired()
+		{
+			_countdown = 0;
+
+			Transform tile = GetDissolvableTile();
+			if ( tile != null )
+			{
+				_builder.RemoveTile( tile );
+			}
+			else
+			{
+				_radius -= _builder.CellSize / 2f;
+			}
+		}
+
+		private Transform GetDissolvableTile()
+		{
+			List<Transform> viableTiles = new List<Transform>();
+
+			float distance = _radius - _distanceThreshold;
+			foreach ( Transform tile in _builder.Tiles )
+			{
+				float distSqr = (tile.position - _builder.transform.position).sqrMagnitude;
+				if ( distSqr >= distance * distance )
+				{
+					viableTiles.Add( tile );
+				}
+			}
+
+			if ( viableTiles.Count <= 0 )
+			{
+				return null;
+			}
+
+			int randIdx = Random.Range( 0, viableTiles.Count );
+			return viableTiles[randIdx];
+		}
+	}
+}
