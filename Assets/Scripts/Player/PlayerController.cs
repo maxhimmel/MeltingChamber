5ecc96d22f3ce10f938bcccecbc5958fa7b67e99
Max <himmelmax@gmail.com>
2022-08-10 18:27:29 -0700

    player can activate the reflector and aim it!
    cleaned up axis input polling

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using MeltingChamber.Gameplay.Movement;
+using MeltingChamber.ReConsts;
 using UnityEngine;
 using Zenject;
 
@@ -9,47 +10,72 @@ namespace MeltingChamber.Gameplay.Player
 		private Rewired.Player _input;
 		private CharacterMotor _motor;
 		private DashController _dashController;
+		private Reflector _reflector;
 
+		private Vector2 _directedAimInput = Vector2.up;
 		private Vector2 _directedMoveInput = Vector2.right;
 
 		[Inject]
 		public void Construct( Rewired.Player input,
 			CharacterMotor motor,
-			DashController dashController )
+			DashController dashController,
+			Reflector reflector )
 		{
 			_input = input;
 			_motor = motor;
 			_dashController = dashController;
+
+			_reflector = reflector;
+			reflector.enabled = false;
 		}
 
 		private void Update()
 		{
+			HandleReflector();
 			HandleMovement();
+		}
+
+		private void HandleReflector()
+		{
+			var aimInput = GetAxisInput( Action.AimHorizontal, Action.AimVertical, ref _directedAimInput );
+			_reflector.transform.rotation = Quaternion.LookRotation( Vector3.forward, aimInput );
+
+			if ( _input.GetButtonDown( Action.Reflect ) )
+			{
+				_reflector.enabled = true;
+			}
+			else if ( _input.GetButtonUp( Action.Reflect ) )
+			{
+				_reflector.enabled = false;
+			}
 		}
 
 		private void HandleMovement()
 		{
-			var moveInput = GetMoveInput();
-			if ( moveInput != Vector2.zero )
-			{
-				_directedMoveInput = moveInput;
-			}
+			var moveInput = GetAxisInput( Action.MoveHorizontal, Action.MoveVertical, ref _directedMoveInput );
 
 			if ( !_dashController.IsDashing )
 			{
 				_motor.SetDesiredVelocity( moveInput );
 
-				if ( _input.GetButtonDown( ReConsts.Action.Dash ) )
+				if ( _input.GetButtonDown( Action.Dash ) )
 				{
 					_dashController.Dash( _directedMoveInput );
 				}
 			}
 		}
 
-		private Vector2 GetMoveInput()
+		private Vector2 GetAxisInput( int horizontalId, int verticalId, ref Vector2 validDirectedInput )
 		{
-			var rawInput = _input.GetAxis2D( ReConsts.Action.MoveHorizontal, ReConsts.Action.MoveVertical );
-			return Vector2.ClampMagnitude( rawInput, 1 );
+			var rawInput = _input.GetAxis2D( horizontalId, verticalId );
+			var result = Vector2.ClampMagnitude( rawInput, 1 );
+
+			if ( result != Vector2.zero )
+			{
+				validDirectedInput = result;
+			}
+
+			return result;
 		}
 	}
 }
