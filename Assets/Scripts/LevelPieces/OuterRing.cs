6f949570225f19c52abc0b9df9ed0be6876d7111
Max <himmelmax@gmail.com>
2022-08-13 09:46:53 -0700

    using edge abstractions

--- Assets/Scripts/LevelPieces/OuterRing.cs
+++ Assets/Scripts/LevelPieces/OuterRing.cs
@@ -1,52 +1,35 @@
 using MeltingChamber.Extensions;
+using MeltingChamber.Utility.Edge;
 using UnityEngine;
 
 namespace MeltingChamber.Gameplay.LevelPieces
 {
 	public class OuterRing : MonoBehaviour
 	{
+		[SerializeField] private RingConfig _ringConfig = new RingConfig();
+
 		[Header( "Collision" )]
 		[SerializeField] private int _resolution = 20;
 		[SerializeField] private float _radius = 10;
 
-		private EdgeCollider2D _edgeCollider;
+		private EdgeBuilder _edgeBuilder;
 
 		private void Start()
 		{
-			GenerateCollision();
-		}
-
-		private void GenerateCollision()
-		{
-			Vector2[] colliderPoints = new Vector2[_resolution + 1];
-
-			float angleStep = 360f / _resolution;
-			for ( int idx = 0; idx <= _resolution; ++idx )
-			{
-				float radian = Mathf.Deg2Rad * (angleStep * idx);
-				Vector2 offsetDir = Vector3.right * Mathf.Cos( radian ) + Vector3.up * Mathf.Sin( radian );
-				Vector2 pos = transform.position.XY() + offsetDir * _radius;
-
-				colliderPoints[idx] = pos;
-			}
-
-			_edgeCollider.points = colliderPoints;
+			_edgeBuilder.Build();
 		}
 
 		private void Awake()
 		{
-			_edgeCollider = GetComponentInChildren<EdgeCollider2D>();
+			_edgeBuilder = new EdgeBuilder(
+				new EdgeRingCollision( GetComponent<EdgeCollider2D>(), _ringConfig ),
+				new EdgeRenderer( GetComponent<LineRenderer>() )
+			);
 		}
-
-#if UNITY_EDITOR
-		[Header( "Editor/Tools" )]
-		[SerializeField] private Color _radiusColor = Color.green;
 
 		private void OnDrawGizmosSelected()
 		{
-			UnityEditor.Handles.color = _radiusColor;
-			UnityEditor.Handles.DrawWireDisc( transform.position, Vector3.back, _radius );
+			_ringConfig.OnDrawGizmos( transform );
 		}
-#endif
 	}
 }
--- Assets/Scripts/Reflector/Reflector.cs
+++ Assets/Scripts/Reflector/Reflector.cs
@@ -1,89 +1,46 @@
+using MeltingChamber.Utility.Edge;
 using UnityEngine;
 
 namespace MeltingChamber.Gameplay
 {
     public class Reflector : MonoBehaviour
     {
-		[Header( "Collision" )]
-		[SerializeField] private int _resolution = 5;
-		[SerializeField] private float _radiusOffset = 0;
-		[SerializeField] private float _diameter = 2.5f;
-		[SerializeField] private Vector3 _centerOffset = new Vector3( 0, 0.75f, 0 );
+		[SerializeField] private ArcConfig _arcConfig = new ArcConfig();
 
-		private EdgeCollider2D _edgeCollider;
-
-		private void Start()
-		{
-			GenerateCollision();
-		}
-
-		private void GenerateCollision()
-		{
-			Vector3 origin = _centerOffset;
-
-			float radius = _diameter / 2f;
-			Vector3 lhsPos = origin - Vector3.right * radius;
-			Vector3 rhsPos = origin + Vector3.right * radius;
-
-			Vector3 centerPivot = origin + Vector3.up * _radiusOffset;
-			Vector3 lhsRelativeCenter = lhsPos - centerPivot;
-			Vector3 rhsRelativeCenter = rhsPos - centerPivot;
-
-			Vector2[] points = new Vector2[_resolution + 1];
-			for ( int idx = 0; idx <= _resolution; ++idx )
-			{
-				float t = idx / (float)_resolution;
-				Vector3 pos = Vector3.Slerp( lhsRelativeCenter, rhsRelativeCenter, t ) + centerPivot;
-
-				points[idx] = pos;
-			}
-
-			_edgeCollider.points = points;
-		}
+		private EdgeBuilder _edgeBuilder;
 
 		private void OnEnable()
 		{
-			_edgeCollider.enabled = true;
+			if ( _edgeBuilder != null && _edgeBuilder.Edge != null )
+			{
+				_edgeBuilder.Edge.enabled = true;
+			}
 		}
 
 		private void OnDisable()
 		{
-			_edgeCollider.enabled = false;
+			if ( _edgeBuilder != null && _edgeBuilder.Edge != null )
+			{
+				_edgeBuilder.Edge.enabled = false;
+			}
+		}
+
+		private void Start()
+		{
+			_edgeBuilder.Build();
 		}
 
 		private void Awake()
 		{
-			_edgeCollider = GetComponentInChildren<EdgeCollider2D>();
+			_edgeBuilder = new EdgeBuilder(
+				new EdgeArcCollision( GetComponent<EdgeCollider2D>(), _arcConfig ),
+				new EdgeRenderer( GetComponent<LineRenderer>() )
+			);
 		}
-
-#if UNITY_EDITOR
-		[Header( "Editor/Tools" )]
-		[SerializeField] private Color _gizmoColor = Color.green;
 
 		private void OnDrawGizmosSelected()
 		{
-			Vector3 origin = transform.position + _centerOffset;
-
-			float radius = _diameter / 2f;
-			Vector3 lhsPos = origin - transform.right * radius;
-			Vector3 rhsPos = origin + transform.right * radius;
-
-			Vector3 centerPivot = origin + transform.up * _radiusOffset;
-			Vector3 lhsRelativeCenter = lhsPos - centerPivot;
-			Vector3 rhsRelativeCenter = rhsPos - centerPivot;
-
-			Gizmos.color = _gizmoColor;
-			for ( int idx = 0; idx < _resolution; ++idx )
-			{
-				float t1 = idx / (float)_resolution;
-				Vector3 pos1 = Vector3.Slerp( lhsRelativeCenter, rhsRelativeCenter, t1 ) + centerPivot;
-
-				float t2 = (idx + 1) / (float)_resolution;
-				Vector3 pos2 = Vector3.Slerp( lhsRelativeCenter, rhsRelativeCenter, t2 ) + centerPivot;
-
-				Gizmos.DrawLine( pos1, pos2 );
-			}
+			_arcConfig.OnDrawGizmos( transform );
 		}
-#endif
 	}
 }
