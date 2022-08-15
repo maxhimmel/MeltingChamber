6192e06c23419032aad9bfbe86644f1256c069df
Max <himmelmax@gmail.com>
2022-08-15 08:07:09 -0700

    pong ball will always move at a constant speed

---
+++ Assets/Scripts/PongBall/PongBall.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Zenject;
+
+namespace MeltingChamber.Gameplay
+{
+    public class PongBall : MonoBehaviour
+    {
+		[SerializeField] private float _moveSpeed = 10;
+
+		private Rigidbody2D _body;
+
+		[Inject]
+		public void Construct( Rigidbody2D body )
+		{
+            _body = body;
+		}
+
+		private void OnCollisionEnter2D( Collision2D collision )
+		{
+			var contact = collision.GetContact( 0 );
+			var reflect = Vector2.Reflect( -collision.relativeVelocity, contact.normal );
+
+			_body.velocity = reflect.normalized * _moveSpeed;
+		}
+	}
+}
