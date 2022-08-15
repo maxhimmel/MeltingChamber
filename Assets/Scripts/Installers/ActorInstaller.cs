96d28b6ed0131b19a33132ecf1e6b4e4d230b84f
Max <himmelmax@gmail.com>
2022-08-14 17:55:27 -0700

    actor installer must reference main collider

--- Assets/Scripts/Installers/ActorInstaller.cs
+++ Assets/Scripts/Installers/ActorInstaller.cs
@@ -5,9 +5,12 @@ namespace MeltingChamber.Installers
 {
     public class ActorInstaller : MonoInstaller
     {
+		[SerializeField] private Collider2D _mainCollider = default;
+
 		public override void InstallBindings()
 		{
 			Container.Bind<Rigidbody2D>().FromMethod( GetComponentInChildren<Rigidbody2D> ).AsSingle();
+			Container.BindInstance( _mainCollider ).AsSingle();
 		}
 	}
 }
