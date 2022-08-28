45a790836ca97e938bd26b289893d08d626445ff
Max <himmelmax@gmail.com>
2022-08-28 16:33:44 -0700

    handling button activations

--- Assets/Scripts/LevelPieces/PongButtonController.cs
+++ Assets/Scripts/LevelPieces/PongButtonController.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using MeltingChamber.Extensions;
 using MeltingChamber.Utility.Edge;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using Zenject;
 
@@ -8,12 +11,19 @@ namespace MeltingChamber.Gameplay.LevelPieces
 {
     public class PongButtonController : MonoBehaviour
     {
+		[Header( "Spawn Config" )]
 		[SerializeField] private int _buttonCount = 5;
 		[SerializeField, Range( 0, 360 )] private float _rotationOffset = 0;
+
+		[Header( "Interactions" )]
+		[PropertyRange( 1, nameof( _buttonCount ) )]
+		[SerializeField] private int _maxActiveButtonCount = 2;
+		[SerializeField] private float _reactivateDelay = 5;
 
 		private PongButton.Factory _buttonFactory;
 		private ArenaDissolver _dissolver;
 		private List<PongButton> _buttons;
+		private int _nextButtonIndex;
 
 		[Inject]
 		public void Construct( PongButton.Factory buttonFactory, 
@@ -31,8 +41,53 @@ namespace MeltingChamber.Gameplay.LevelPieces
 			{
 				var newButton = CreateButton( spawnPos );
 
+				newButton.enabled = false;
 				newButton.Triggered += OnButtonTriggered;
+
 				_buttons.Add( newButton );
+			}
+
+			InitializeFirstActiveButtons();
+		}
+
+		private void InitializeFirstActiveButtons()
+		{
+			_buttons.Shuffle();
+			_nextButtonIndex = 0;
+
+			for ( int idx = 0; idx < _maxActiveButtonCount; ++idx )
+			{
+				var button = GetNextButton();
+				button.enabled = true;
+			}
+		}
+
+		private PongButton GetNextButton()
+		{
+			var result = _buttons[_nextButtonIndex];
+			
+			++_nextButtonIndex;
+			if ( _nextButtonIndex >= _buttons.Count )
+			{
+				_nextButtonIndex = 0;
+				_buttons.Shuffle();
+			}
+
+			return result;
+		}
+
+		private IEnumerable<Vector3> GetSpawnPositions( float radius )
+		{
+			Vector3 origin = transform.position;
+			for ( int idx = 0; idx < _buttonCount; ++idx )
+			{
+				float percentage = (idx + 1) / (float)_buttonCount;
+				float radian = Mathf.Deg2Rad * (360f * percentage + _rotationOffset);
+
+				Vector3 offset = transform.right * Mathf.Cos( radian ) + transform.up * Mathf.Sin( radian );
+				Vector3 spawnPos = origin + offset * radius;
+
+				yield return spawnPos;
 			}
 		}
 
@@ -53,22 +108,17 @@ namespace MeltingChamber.Gameplay.LevelPieces
 			{
 				_dissolver.ResetCountdown();
 				button.enabled = false;
+
+				HandleReactivation();
 			}
 		}
 
-		private IEnumerable<Vector3> GetSpawnPositions( float radius )
+		private async void HandleReactivation()
 		{
-			Vector3 origin = transform.position;
-			for ( int idx = 0; idx < _buttonCount; ++idx )
-			{
-				float percentage = (idx + 1) / (float)_buttonCount;
-				float radian = Mathf.Deg2Rad * (360f * percentage + _rotationOffset);
+			await Task.Delay( TimeSpan.FromSeconds( _reactivateDelay ) );
 
-				Vector3 offset = transform.right * Mathf.Cos( radian ) + transform.up * Mathf.Sin( radian );
-				Vector3 spawnPos = origin + offset * radius;
-
-				yield return spawnPos;
-			}
+			var button = GetNextButton();
+			button.enabled = true;
 		}
 
 
