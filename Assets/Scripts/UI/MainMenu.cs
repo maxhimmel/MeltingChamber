1fa50f6bacb1b6fa2a902328510110abab66e2e8
Max <himmelmax@gmail.com>
2022-08-29 08:18:24 -0700

    added quit button to main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,6 +8,7 @@ namespace MeltingChamber.UI
     public class MainMenu : MonoBehaviour
     {
 		[SerializeField] private Button _startButton;
+		[SerializeField] private Button _exitButton;
 
 		private LevelManager _levelManager;
 
@@ -20,12 +21,22 @@ namespace MeltingChamber.UI
 		private void Start()
 		{
 			_startButton.onClick.AddListener( LoadNextLevel );
+			_exitButton.onClick.AddListener( Quit );
 		}
 
 		private async void LoadNextLevel()
 		{
 			_startButton.interactable = false;
 			await _levelManager.LoadNextLevel();
+		}
+
+		private void Quit()
+		{
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.ExitPlaymode();
+#else
+			Application.Quit();
+#endif
 		}
 	}
 }
